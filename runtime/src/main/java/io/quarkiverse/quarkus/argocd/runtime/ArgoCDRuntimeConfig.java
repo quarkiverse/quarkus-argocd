d80d70b434b7ff6381eb31a969b6298a24cf5d6f
Ioannis Canellos <iocanel@gmail.com>
2024-10-24 18:01:54 +0300

    chore: more workaround javadoc and fixes issues for release

---
+++ runtime/src/main/java/io/quarkiverse/quarkus/argocd/runtime/ArgoCDRuntimeConfig.java
@@ -0,0 +1,13 @@
+package io.quarkiverse.quarkus.argocd.runtime;
+
+import io.quarkus.runtime.annotations.ConfigPhase;
+import io.quarkus.runtime.annotations.ConfigRoot;
+import io.smallrye.config.ConfigMapping;
+
+/**
+ * ArgoCD runtime configuration
+ */
+@ConfigMapping(prefix = "quarkus.argocd")
+@ConfigRoot(phase = ConfigPhase.RUN_TIME)
+public interface ArgoCDRuntimeConfig {
+}
