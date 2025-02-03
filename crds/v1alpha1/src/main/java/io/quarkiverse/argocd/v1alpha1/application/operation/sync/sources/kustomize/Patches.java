ca3bdce98b2271637d362672bcaf85f58ceb79c7
cmoulliard <cmoulliard@redhat.com>
2025-02-03 17:25:35 +0100

    Bump the version of the f8 kube client to generate the non-standard CRD java classes. #71
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

---
+++ crds/v1alpha1/src/main/java/io/quarkiverse/argocd/v1alpha1/application/operation/sync/sources/kustomize/Patches.java
@@ -0,0 +1,73 @@
+package io.quarkiverse.argocd.v1alpha1.application.operation.sync.sources.kustomize;
+
+@com.fasterxml.jackson.annotation.JsonInclude(com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL)
+@com.fasterxml.jackson.annotation.JsonPropertyOrder({ "options", "patch", "path", "target" })
+@com.fasterxml.jackson.databind.annotation.JsonDeserialize(using = com.fasterxml.jackson.databind.JsonDeserializer.None.class)
+@lombok.ToString()
+@lombok.EqualsAndHashCode()
+@io.sundr.builder.annotations.Buildable(editableEnabled = false, validationEnabled = false, generateBuilderPackage = false, builderPackage = "io.fabric8.kubernetes.api.builder", refs = {
+        @io.sundr.builder.annotations.BuildableReference(io.fabric8.kubernetes.api.model.ObjectMeta.class),
+        @io.sundr.builder.annotations.BuildableReference(io.fabric8.kubernetes.api.model.ObjectReference.class),
+        @io.sundr.builder.annotations.BuildableReference(io.fabric8.kubernetes.api.model.LabelSelector.class),
+        @io.sundr.builder.annotations.BuildableReference(io.fabric8.kubernetes.api.model.Container.class),
+        @io.sundr.builder.annotations.BuildableReference(io.fabric8.kubernetes.api.model.EnvVar.class),
+        @io.sundr.builder.annotations.BuildableReference(io.fabric8.kubernetes.api.model.ContainerPort.class),
+        @io.sundr.builder.annotations.BuildableReference(io.fabric8.kubernetes.api.model.Volume.class),
+        @io.sundr.builder.annotations.BuildableReference(io.fabric8.kubernetes.api.model.VolumeMount.class)
+})
+public class Patches implements io.fabric8.kubernetes.api.builder.Editable<PatchesBuilder>,
+        io.fabric8.kubernetes.api.model.KubernetesResource {
+
+    @java.lang.Override
+    public PatchesBuilder edit() {
+        return new PatchesBuilder(this);
+    }
+
+    @com.fasterxml.jackson.annotation.JsonProperty("options")
+    @com.fasterxml.jackson.annotation.JsonSetter(nulls = com.fasterxml.jackson.annotation.Nulls.SKIP)
+    private java.util.Map<java.lang.String, Boolean> options;
+
+    public java.util.Map<java.lang.String, Boolean> getOptions() {
+        return options;
+    }
+
+    public void setOptions(java.util.Map<java.lang.String, Boolean> options) {
+        this.options = options;
+    }
+
+    @com.fasterxml.jackson.annotation.JsonProperty("patch")
+    @com.fasterxml.jackson.annotation.JsonSetter(nulls = com.fasterxml.jackson.annotation.Nulls.SKIP)
+    private String patch;
+
+    public String getPatch() {
+        return patch;
+    }
+
+    public void setPatch(String patch) {
+        this.patch = patch;
+    }
+
+    @com.fasterxml.jackson.annotation.JsonProperty("path")
+    @com.fasterxml.jackson.annotation.JsonSetter(nulls = com.fasterxml.jackson.annotation.Nulls.SKIP)
+    private String path;
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    @com.fasterxml.jackson.annotation.JsonProperty("target")
+    @com.fasterxml.jackson.annotation.JsonSetter(nulls = com.fasterxml.jackson.annotation.Nulls.SKIP)
+    private io.quarkiverse.argocd.v1alpha1.application.operation.sync.sources.kustomize.patches.Target target;
+
+    public io.quarkiverse.argocd.v1alpha1.application.operation.sync.sources.kustomize.patches.Target getTarget() {
+        return target;
+    }
+
+    public void setTarget(io.quarkiverse.argocd.v1alpha1.application.operation.sync.sources.kustomize.patches.Target target) {
+        this.target = target;
+    }
+}
---
+++ crds/v1alpha1/src/main/java/io/quarkiverse/argocd/v1alpha1/application/operation/sync/syncstrategy/Hook.java
@@ -0,0 +1,42 @@
+package io.quarkiverse.argocd.v1alpha1.application.operation.sync.syncstrategy;
+
+@com.fasterxml.jackson.annotation.JsonInclude(com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL)
+@com.fasterxml.jackson.annotation.JsonPropertyOrder({ "force" })
+@com.fasterxml.jackson.databind.annotation.JsonDeserialize(using = com.fasterxml.jackson.databind.JsonDeserializer.None.class)
+@lombok.ToString()
+@lombok.EqualsAndHashCode()
+@io.sundr.builder.annotations.Buildable(editableEnabled = false, validationEnabled = false, generateBuilderPackage = false, builderPackage = "io.fabric8.kubernetes.api.builder", refs = {
+        @io.sundr.builder.annotations.BuildableReference(io.fabric8.kubernetes.api.model.ObjectMeta.class),
+        @io.sundr.builder.annotations.BuildableReference(io.fabric8.kubernetes.api.model.ObjectReference.class),
+        @io.sundr.builder.annotations.BuildableReference(io.fabric8.kubernetes.api.model.LabelSelector.class),
+        @io.sundr.builder.annotations.BuildableReference(io.fabric8.kubernetes.api.model.Container.class),
+        @io.sundr.builder.annotations.BuildableReference(io.fabric8.kubernetes.api.model.EnvVar.class),
+        @io.sundr.builder.annotations.BuildableReference(io.fabric8.kubernetes.api.model.ContainerPort.class),
+        @io.sundr.builder.annotations.BuildableReference(io.fabric8.kubernetes.api.model.Volume.class),
+        @io.sundr.builder.annotations.BuildableReference(io.fabric8.kubernetes.api.model.VolumeMount.class)
+})
+public class Hook
+        implements io.fabric8.kubernetes.api.builder.Editable<HookBuilder>, io.fabric8.kubernetes.api.model.KubernetesResource {
+
+    @java.lang.Override
+    public HookBuilder edit() {
+        return new HookBuilder(this);
+    }
+
+    /**
+     * Force indicates whether or not to supply the --force flag to `kubectl apply`. The --force flag deletes and re-create the
+     * resource, when PATCH encounters conflict and has retried for 5 times.
+     */
+    @com.fasterxml.jackson.annotation.JsonProperty("force")
+    @com.fasterxml.jackson.annotation.JsonPropertyDescription("Force indicates whether or not to supply the --force flag to `kubectl apply`. The --force flag deletes and re-create the resource, when PATCH encounters conflict and has retried for 5 times.")
+    @com.fasterxml.jackson.annotation.JsonSetter(nulls = com.fasterxml.jackson.annotation.Nulls.SKIP)
+    private Boolean force;
+
+    public Boolean getForce() {
+        return force;
+    }
+
+    public void setForce(Boolean force) {
+        this.force = force;
+    }
+}
