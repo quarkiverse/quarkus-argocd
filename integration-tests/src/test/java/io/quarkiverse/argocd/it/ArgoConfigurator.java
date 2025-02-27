41e2b45be96c33f4b8c3c268eae0ae8ad972ae76
cmoulliard <cmoulliard@redhat.com>
2025-02-27 15:45:53 +0100

    Replace \@Builder.Default with \@Default
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

--- integration-tests/src/test/java/io/quarkiverse/argocd/it/ArgoConfigurator.java
+++ integration-tests/src/test/java/io/quarkiverse/argocd/it/ArgoConfigurator.java
@@ -1,11 +1,11 @@
 package io.quarkiverse.argocd.it;
 
-import lombok.Builder.Default;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Builder.Default;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Builder
 @Getter
