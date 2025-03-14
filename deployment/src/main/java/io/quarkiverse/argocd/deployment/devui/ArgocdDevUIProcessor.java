7b1fbc03177dec63555322fb0dcd4ae37d2dcfa6
cmoulliard <cmoulliard@redhat.com>
2025-03-14 17:50:13 +0100

    Adapt the code to avoid to throw an exception if Argocd is not enabled and show a warning message. #80
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

--- deployment/src/main/java/io/quarkiverse/argocd/deployment/devui/ArgocdDevUIProcessor.java
+++ deployment/src/main/java/io/quarkiverse/argocd/deployment/devui/ArgocdDevUIProcessor.java
@@ -19,30 +19,31 @@ public class ArgocdDevUIProcessor {
     @BuildStep(onlyIf = IsDevelopment.class)
     void createCard(Optional<ArgocdDevServiceInfoBuildItem> info, BuildProducer<CardPageBuildItem> cardPage) {
 
-        CardPageBuildItem card = new CardPageBuildItem();
-        LOG.debug("Creating card page");
-
         info.ifPresent(i -> {
+            CardPageBuildItem card = new CardPageBuildItem();
+            LOG.debug("Creating card page");
+
             String url = String.format("https://%s:%s", i.host(), i.hostPort());
             LOG.debug("Creating an external link page for: argocd UI");
             card.addPage(Page.externalPageBuilder("Argocd Dashboard")
                     .doNotEmbed()
                     .icon("font-awesome-solid:code-branch")
                     .url(url));
+
+            LOG.debug("Creating an external link page for: argocd project & version");
+            final ExternalPageBuilder versionPage = Page.externalPageBuilder("Argocd project")
+                    .icon("font-awesome-solid:tag")
+                    .url("https://argo-cd.readthedocs.io/en/stable/")
+                    .doNotEmbed()
+                    .staticLabel("2.13.2");
+
+            LOG.debug("Add version page");
+            card.addPage(versionPage);
+            LOG.debug("Set custom car with js");
+            card.setCustomCard("qwc-argocd-card.js");
+            LOG.debug("Produce ...");
+            cardPage.produce(card);
+
         });
-
-        LOG.debug("Creating an external link page for: argocd project & version");
-        final ExternalPageBuilder versionPage = Page.externalPageBuilder("Argocd project")
-                .icon("font-awesome-solid:tag")
-                .url("https://argo-cd.readthedocs.io/en/stable/")
-                .doNotEmbed()
-                .staticLabel("2.13.2");
-
-        LOG.debug("Add version page");
-        card.addPage(versionPage);
-        LOG.debug("Set custom car with js");
-        card.setCustomCard("qwc-argocd-card.js");
-        LOG.debug("Produce ...");
-        cardPage.produce(card);
     }
 }
